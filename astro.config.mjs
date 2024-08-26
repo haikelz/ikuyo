6c61570ad89182aff7a95a7a2f38b5263d06516e
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-08-26 15:14:23 +0700

    feat: pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import rehypePresetMinify from "rehype-preset-minify";
-import remarkToc from "remark-toc";
-import react from "@astrojs/react";
 import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx({
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "vitesse-dark"
-    },
-    remarkPlugins: [remarkToc],
-    rehypePlugins: [rehypePresetMinify, rehypeSlug],
-    remarkRehype: {
-      footnoteLabel: "Footnotes"
-    },
-    gfm: true,
-    optimize: true
-  }), react(), icon()]
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "vitesse-dark",
+      },
+      remarkPlugins: [remarkToc, remarkReadingTime],
+      rehypePlugins: [rehypePresetMinify, rehypeSlug],
+      remarkRehype: {
+        footnoteLabel: "Footnotes",
+      },
+      gfm: true,
+      optimize: true,
+    }),
+    react(),
+    icon(),
+  ],
 });
