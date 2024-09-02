f367ecdf822f48628c6efcc1b24d9969cb76f8bf
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-09-02 07:19:21 +0700

    fix: guestbook

--- src/pages/api/guestbook.ts
+++ src/pages/api/guestbook.ts
@@ -3,7 +3,15 @@ import { prisma } from "~lib/utils/prisma";
 
 export const GET: APIRoute = async () => {
   try {
-    const data = await prisma.guestbook.findMany();
+    const data = await prisma.guestbook.findMany({
+      select: {
+        id: true,
+        created_at: true,
+        email: false,
+        username: true,
+        message: true,
+      },
+    });
 
     return new Response(
       JSON.stringify({
