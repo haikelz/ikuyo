03db6f6ddb0cdc94bbf2dfba41663ba3f06ec8ac
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-03-08 14:08:54 +0700

    hotfix: toc

--- apps/web/src/components/svelte/TOC.svelte
+++ apps/web/src/components/svelte/TOC.svelte
@@ -21,7 +21,7 @@
   ><Menu size={21} />
 </button>
 <div
-  class="right-4 rounded-md p-4 toc bottom-4 z-50 bg-monochrome-1 slide-enter-content border border-monochrome-3"
+  class="right-4 rounded-md backdrop-blur-md p-4 toc bottom-4 z-50 bg-monochrome-1/70 slide-enter-content border border-monochrome-3"
   class:fixed={isOpen}
   class:hidden={!isOpen}
 >
