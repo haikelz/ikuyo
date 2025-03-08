2bf2142953a33f5d6583448ff65f395fa932eaaa
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-03-08 14:24:19 +0700

    feat: reading indicator

--- apps/web/src/components/svelte/ReadingIndicator.svelte
+++ apps/web/src/components/svelte/ReadingIndicator.svelte
@@ -29,6 +29,6 @@
 {#if mounted}
   <div
     style="width: {width}%"
-    class="fixed h-0.5 bg-monochrome-4 z-50 reading-progress"
+    class="fixed h-1 bg-monochrome-4 z-50 reading-progress"
   ></div>
 {/if}
